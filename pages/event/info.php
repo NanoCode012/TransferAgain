593a83e55620ab7ef1a59e21e5bda7d7a07e6ae1
NanoCode012 <kevinvong@rocketmail.com>
2021-01-23 01:03:41 +0700

    Remove

--- pages/event/info.php
+++
@@ -1,7 +0,0 @@
-<div class="wrapper">
-    <h2>My View</h2>
-    <p>
-        <a href="" class="btn btn-primary">Expense History</a>
-        <a href="?p=view" class="btn btn-dark">Back</a>
-    </p>
-</div>
