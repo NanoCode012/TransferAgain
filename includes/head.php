de1f1df5defe7e03f0b86ff457bb0aaedeaf19b2
NanoCode012 <kevinvong@rocketmail.com>
2021-01-23 18:10:16 +0700

    Change jquery to min cdn

--- includes/head.php
+++ includes/head.php
@@ -24,6 +24,6 @@ include 'utils/functions.php';
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.1/dist/bootstrap-table.min.css">
 
     <!-- JQuery -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <title><?php echo $servertitle; ?></title>
 </head>
